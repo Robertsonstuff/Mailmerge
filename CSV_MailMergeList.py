a63c9ad99c58d6d388434337f5126b427c88d9b5
luke robertson <robertson.stuff@gmail.com>
2021-08-31 14:07:42 +1000

    Update CSV_MailMergeList.py

--- CSV_MailMergeList.py
+++ CSV_MailMergeList.py
@@ -1,21 +1,14 @@
 import csv
 import pyperclip
 
-f = open('mailmergelist.csv', 'r')
+with open("blank.csv", "r") as file:
+    reader = csv.reader(file)
+    particulars = []
+    for part in reader:
+        particulars.append(f"hi {part[0]}, please do {part[1]} and then {part[2]}.\nThanks for your help! ")
+    for item in particulars:
+        print(item)
+	pyperclip.copy(item)
+        input("Copied to clipboard..")
 
-reader = csv.reader(f)
-
-Particulars = []
-
-for row in reader:
-    Particulars.append('Hi ' + (row[3]) + ' I am missing device' + (row[5]) + " from ticket " + (row[4]))
-
-print("Let's start our mailmerge:")
-
-for item in Particulars:
-    print(item)
-    pyperclip.copy(item)
-    input("Copied to clipboard..")
-
-	
-
+print("\ndone\n")
